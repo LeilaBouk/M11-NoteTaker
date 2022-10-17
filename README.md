b1159d6b1752189a22fe8327f90c30f7ba44fdfe
Leila <leila.boukella@gmail.com>
2022-10-17 00:18:53 -0700

    almost works

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # M11-NoteTaker
 
 📝
+
+## Website screenshot 📸
+
+![Website screenshot](./images/screenshot.png)
+
+## Deployed website link 🤖
+
+-
+
+## About this project 🐊
+
+Note taking app project using Express.js backend. 
+
+## Languages 💬
+HTML + CSS + JavaScript
+
+## Usage 🐗
+
+Note taking app! Save or delete notes. 
--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -5,23 +5,34 @@ const noteData = require("../db/db.json");
 const router = express.Router();
 const uuid = require('../helpers/uuid.js')
 
-router.get('/api/notes', (req, res) => {
-    fs.readFile(noteData).then((data) => res.json(JSON.parse(data)));
+router.get('/notes', (req, res) => {
+    res.json(noteData)
     console.log("Note data found 📝")
 });
 
-router.post('/api/notes', (req, res) => {
-    fs.readFile(noteData).then((data) => res.json(JSON.parse(data)));
-    const { title, text, id } = req.body;
-    if (title && text && id) {
-        const userNote = {
-            title,
-            text,
-            id: uuid(),
-        }
+router.post('/notes', (req, res) => {
+    console.log("note taken");
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        let myNote = JSON.parse(data);
+        const { title, text, id } = req.body;
+        if (title && text) {
+            const userNote = {
+                title,
+                text,
+                id: uuid(),
+            };
+            let parseNote = JSON.parse(data);
+            parseNote.push(myNote)
 
-        readAndAppend(userNote, noteData);
-    }
+            fs.writeFile('./db/db.json', JSON.stringify(parseNote), (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(noteData);
+                }
+            })
+        }
+    })
 })
 
 module.exports = router;
